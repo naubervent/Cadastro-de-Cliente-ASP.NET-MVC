66ff4e0bea3285c91821b328060193a937bca022
Nauber Ventura <naubervent@gmail.com>
2017-02-21 16:57:48 -0300

    Cadastro de Cliente

---
+++ ClienteController.cs
@@ -0,0 +1,120 @@
+﻿using PizzariaSys.Dominio;
+using PizzariaSys.Dominio.Interfaces;
+using PizzariaSys.Dominio.Negocios;
+using PizzariaSys.Web.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PizzariaSys.Web.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private IClienteNegocios _clienteNegocios = new ClienteNegocios();
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            var clientes = _clienteNegocios.ListarTodos();
+            return View(clientes);
+        }
+
+        [HttpGet]
+        public ActionResult Inserir()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Inserir(ClienteViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var cliente = new Cliente
+            {
+                Nome = model.Nome,
+                Logradouro = model.Logradouro,
+                Numero = model.Numero,
+                Bairro = model.Bairro,
+                Telefone = model.Telefone
+            };
+
+            _clienteNegocios.Salvar(cliente);
+
+            return RedirectToAction("Index");           
+        }
+
+        [HttpGet]
+        public ActionResult ConsultaTelefone()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ConsultaTelefone(string telefone) 
+        {
+            var cliente = _clienteNegocios.ListarClienteTelefone(telefone);
+            if (cliente == null)
+            {
+                return View();
+            }
+            return View("Detalhes", new ClienteViewModel(cliente));
+        }
+
+        [HttpGet]
+        public ActionResult Detalhes(int id)
+        {
+            var cliente = _clienteNegocios.BuscarId(id);
+            return View(new ClienteViewModel(cliente));
+        }
+
+        [HttpGet]
+        public ActionResult Editar(int id)
+        {
+            var cliente = _clienteNegocios.BuscarId(id);
+            return View(new ClienteViewModel(cliente));
+        }
+
+        [HttpPost]
+        public ActionResult Editar(ClienteViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var cliente = new Cliente
+            {
+                Id = model.Id,
+                Nome = model.Nome,
+                Logradouro = model.Logradouro,
+                Numero = model.Numero,
+                Bairro = model.Bairro,
+                Telefone = model.Telefone
+            };
+
+            _clienteNegocios.Salvar(cliente);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Excluir(int id)
+        {
+            var cliente = _clienteNegocios.BuscarId(id);
+            return View(new ClienteViewModel(cliente));
+        }
+
+        [HttpPost, ActionName("Excluir")]
+        public ActionResult ExcluirConfirma(int id)
+        {
+            var cliente = _clienteNegocios.BuscarId(id);
+            _clienteNegocios.Deletar(cliente);       
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ClienteViewModel.cs
@@ -0,0 +1,45 @@
+﻿using PizzariaSys.Dominio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PizzariaSys.Web.ViewModels
+{
+    public class ClienteViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")] 
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Logradouro { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [DataType(DataType.Text)]
+        [RegularExpression(@"^[A-Za-z0-9]*\d+[A-Za-z0-9]*$",ErrorMessage = "Digite somente números")]
+        public int Numero { get; set; }
+
+        public string Bairro { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Telefone { get; set; }
+
+        public ClienteViewModel()
+        {
+
+        }
+
+        public ClienteViewModel(Cliente cliente)
+        {
+            Id = cliente.Id;
+            Nome = cliente.Nome;
+            Logradouro = cliente.Logradouro;
+            Numero = cliente.Numero;
+            Bairro = cliente.Bairro;
+            Telefone = cliente.Telefone;
+        }
+    }
+}
